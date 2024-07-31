167624b4af4219a6e52f56fb329ae36fed650d75
Helene <878723827@qq.com>
2024-08-01 01:51:05 +0800

    lucia: login and signup

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,8 @@ const nextConfig = {
         staleTimes: {
             dynamic: 30
         }
-    }
+    },
+    serverExternalPackages: ["@node-rs/argon2"]
 };
 
 export default nextConfig;
-
-//npm i lucia @lucia-auth/adapter-prisma prisma @prisma/client @tanstack/react-query @tanstack/react-query-devtools @tiptap/react @tiptap/starter-kit @tiptap/extension-placeholder @tiptap/pm uploadthing @uploadthing/react arctic date-fns ky next-themes react-cropper react-image-file-resizer react-intersection-observer react-linkify-it stream-chat stream-chat-react
-
-//npm i -D prettier eslint-config-prettier prettier-plugin-tailwindcss
-
-//npx shadcn-ui@latest add button dialog dropdown-menu form input label skeleton tabs textarea toast tooltip
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
-import { type ClassValue, clsx } from "clsx"
+//A tiny (239B) utility for constructing className strings conditionally.
+import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
